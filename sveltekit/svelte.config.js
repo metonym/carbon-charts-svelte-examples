bcdcffc98d19adbecbb94d2425220fce9abf9bd0
Eric Y Liu <ericyl.us@gmail.com>
2021-07-27 12:06:12 -0700

    fix(sveltekit): do not externalize @carbon/charts when building using a svelte adapter

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,9 +1,19 @@
+import adapter from "@sveltejs/adapter-node";
+
+const production = process.env.NODE_ENV === "production";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    adapter: adapter(),
     target: "#svelte",
     vite: {
-      optimizeDeps: { include: ["@carbon/charts"] },
+      optimizeDeps: {
+        include: ["@carbon/charts"],
+      },
+      ssr: {
+        noExternal: [production && "@carbon/charts"].filter(Boolean),
+      },
     },
   },
 };
