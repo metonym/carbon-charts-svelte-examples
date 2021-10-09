06b38911f05e54b75a96dbf9c88ec0df1553bda8
Eric Liu <ericyl.us@gmail.com>
2021-10-09 15:32:43 -0700

    chore(rollup): upgrade rollup example

--- rollup/src/App.svelte
+++ rollup/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import "@carbon/charts/styles.min.css";
+  import "carbon-components/css/carbon-components.min.css";
   import Basic from "./Basic.svelte";
   import DynamicImport from "./DynamicImport.svelte";
   import Events from "./Events.svelte";
