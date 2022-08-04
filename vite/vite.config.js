e8fe254f5b1f0a4a184d37be775f227adcdfd030
metonym <ericyl.us@gmail.com>
2022-08-04 08:39:12 -0700

    chore(vite): simplify config

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,10 +1,7 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [svelte()],
-    build: { minify: mode === "production" },
-    optimizeDeps: { include: ["@carbon/charts"] },
-  };
-});
+/** @type {import('vite').UserConfig} */
+export default {
+  plugins: [svelte()],
+  optimizeDeps: { include: ["@carbon/charts"] },
+};
