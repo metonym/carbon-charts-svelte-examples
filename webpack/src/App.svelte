bf8e62efe210fc367b9f63c4415c6bfe91c2dfc4
metonym <ericyl.us@gmail.com>
2022-08-09 11:02:44 -0700

    chore(webpack): remove @carbon/styles as an explicit dependency (#2)

--- webpack/src/App.svelte
+++ webpack/src/App.svelte
@@ -1,8 +1,6 @@
 <script>
   import "@carbon/styles/css/styles.css";
   import "@carbon/charts/styles.css";
-
-  //import "carbon-components/css/carbon-components.min.css";
   import Basic from "./Basic.svelte";
   import DynamicImport from "./DynamicImport.svelte";
   import Events from "./Events.svelte";
