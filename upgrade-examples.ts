b41a9d177868d7591184d5390bc4ebf64117243b
Eric Liu <ericyl.us@gmail.com>
2025-02-09 09:18:46 -0800

    chore: also run `bun install` to update lockfile

--- upgrade-examples.ts
+++ upgrade-examples.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
 
 for await (const dir of $`find . -maxdepth 1 -type d -exec test -f {}/package.json \; -print`.lines()) {
-  await $`cd ${dir} && bun update`;
+  await $`cd ${dir} && bun update && bun install`;
 }
